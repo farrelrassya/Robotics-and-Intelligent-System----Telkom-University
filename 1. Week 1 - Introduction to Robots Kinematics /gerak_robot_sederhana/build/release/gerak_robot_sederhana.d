76099ea44b85061a3afdbd8b4c6ab1a9b92a3848
Farrel Rassya <91480200+farrelrassya@users.noreply.github.com>
2023-11-22 12:07:38 +0700

    Delete 1. Week 1 - Introduction to Robots Kinematics /gerak_robot_sederhana/build/release/gerak_robot_sederhana.d

--- 1. Week 1 - Introduction to Robots Kinematics /gerak_robot_sederhana/build/release/gerak_robot_sederhana.d
+++
@@ -1,5 +0,0 @@
-build/release/gerak_robot_sederhana.o: gerak_robot_sederhana.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h
